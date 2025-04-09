0bb2e7dee447fce3dbbaf0b47ec8b217d0b41a12
aitoAarni <aarni.rechardt@gmail.com>
2025-04-10 00:55:07 +0200

    progress

--- is2/is.cc
+++ is2/is.cc
@@ -29,15 +29,47 @@ Result segment(int ny, int nx, const float *data) {
             int baseIndex = x*3 + y*nx*3;
             std::cout << data[baseIndex] << " ";
             double sum_r = 0;
+            double sum_g = 0;
+            double sum_b = 0;
+            double square_sum_r = 0;
+            double square_sum_g = 0;
+            double square_sum_b = 0;
             if (x > 0) {
                 sum_r += r_rec_sum[y][x-1];
+                sum_g += g_rec_sum[y][x-1];
+                sum_b += b_rec_sum[y][x-1];
+                square_sum_r += r_sum_square[y][x-1];
+                square_sum_g += g_sum_square[y][x-1];
+                square_sum_b += b_sum_square[y][x-1];
             }
             if (y > 0) {
                 sum_r += r_rec_sum[y-1][x];
-                if (x > 0) sum_r -= r_rec_sum[y-1][x-1];
+                sum_g += g_rec_sum[y-1][x];
+                sum_b += b_rec_sum[y-1][x];
+                square_sum_r += r_sum_square[y-1][x];
+                square_sum_g += g_sum_square[y-1][x];
+                square_sum_b += b_sum_square[y-1][x];
+                if (x > 0) {
+                    sum_r -= r_rec_sum[y-1][x-1];
+                    sum_g -= g_rec_sum[y-1][x-1];
+                    sum_b -= b_rec_sum[y-1][x-1];
+                    square_sum_r += r_sum_square[y-1][x-1];
+                    square_sum_g += g_sum_square[y-1][x-1];
+                    square_sum_b += b_sum_square[y-1][x-1];
+                }
             }
             sum_r += data[baseIndex];
+            sum_g += data[baseIndex + 1];
+            sum_b += data[baseIndex + 2];
+            square_sum_r += data[baseIndex] * data[baseIndex];
+            square_sum_g += data[baseIndex + 1] * data[baseIndex + 1];
+            square_sum_b += data[baseIndex + 2] * data[baseIndex + 2];
             r_rec_sum[y][x] = sum_r;
+            g_rec_sum[y][x] = sum_g;
+            b_rec_sum[y][x] = sum_b;
+            r_sum_square[y][x] = square_sum_r;
+            g_sum_square[y][x] = square_sum_g;
+            b_sum_square[y][x] = square_sum_b;
         }
     }
     std::cout << "\n\rectangles\n";
