f6f63d50af585fe674a2772297da8b0a6e4ce5f7
aitoAarni <aarni.rechardt@gmail.com>
2025-04-09 17:18:14 +0200

    save point

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+double zeroNormalized[16000000];
+double squareNormalized[16000000];
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -11,29 +13,44 @@ This is the function you need to implement. Quick reference:
 */
 void correlate(int ny, int nx, const float *data, float *result) {
     std::vector<double> means(ny, 0.0);
-    std::vector<double> sd(ny, 0.0);
-
+    std::vector<double> squareSums(ny, 0.0);
     for (int y = 0; y < ny; y++) {
         double sum = 0;
+        std::cout << "row: " << y << "\n";
         for (int x = 0; x < nx; x++) {
+            std::cout << data[x+y*nx] << " ";
             sum += data[x+y*nx];
         }
         means[y] = sum / nx;
+        std::cout << "mean: " << means[y] << "\n\n";
     }
     
+    std::cout << "normalized \n"; 
+    for (int y = 0; y < ny; y++) {
+        std::cout << "row: " << y << "\n";
+        for (int x = 0; x < nx; x++) {
+           zeroNormalized[x+y*nx] = data[x+y*nx] - means[y]; 
+           std::cout << zeroNormalized[x+y*nx] << " ";
+        }
+        std::cout << "\n";
+    }
     for (int y = 0; y < ny; y++) {
         for (int x = 0; x < nx; x++) {
-                sd[y] += std::pow(data[x+y*nx] - means[y], 2);
+            squareSums[y] += std::pow(zeroNormalized[x+y*nx], 2);           
         }
     }
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            squareNormalized[y] = zeroNormalized[x+y*nx] - (squareSums[y] - 1);
+        }
+    }
+
     for (int j = 0; j < ny; j++) {
-        for (int i = j; i < ny; i++) {
-            double numerator = 0;
+        for (int i = 0; i < nx; i++) {
+                result[i + nx*j] = 0;
             for (int x = 0; x < nx; x++) {
-                numerator += (data[x+j*nx] - means[j]) * (data[x+i*nx] - means[i]);
+                result[i + nx*j] += squareNormalized[x + nx * j] * squareNormalized[x+ nx * i];
             }
-            double denominator = std::sqrt(sd[j]) * std::sqrt(sd[i]);
-            result[i+j*ny] = (float)(numerator / denominator);
         }
     }
 }
