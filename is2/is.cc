50e525114b405237f9ad5e0133c938f57206f755
aitoAarni <aarni.rechardt@gmail.com>
2025-04-10 00:01:31 +0200

    save point

--- is2/is.cc
+++ is2/is.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 struct Result {
     int y0;
     int x0;
@@ -7,6 +8,12 @@ struct Result {
     float inner[3];
 };
 
+double r_rec_sum[50][50];
+double g_rec_sum[50][50];
+double b_rec_sum[50][50];
+double r_sum_square[50][50];
+double g_sum_square[50][50];
+double b_sum_square[50][50];
 /*
 This is the function you need to implement. Quick reference:
 - x coordinates: 0 <= x < nx
@@ -15,6 +22,31 @@ This is the function you need to implement. Quick reference:
 - input: data[c + 3 * x + 3 * nx * y]
 */
 Result segment(int ny, int nx, const float *data) {
+    r_rec_sum[0][0] = data[0];
+    for (int y = 0; y<ny; y++) {
+        std::cout << "\n";
+        for (int x = 0; x < nx; x++) {
+            int baseIndex = x*3 + y*nx*3;
+            std::cout << data[baseIndex] << " ";
+            double sum_r = 0;
+            if (x > 0) {
+                sum_r += r_rec_sum[y][x-1];
+            }
+            if (y > 0) {
+                sum_r += r_rec_sum[y-1][x];
+                if (x > 0) sum_r -= r_rec_sum[y-1][x-1];
+            }
+            sum_r += data[baseIndex];
+            r_rec_sum[y][x] = sum_r;
+        }
+    }
+    std::cout << "\n\rectangles\n";
+    for (int y = 0; y<ny; y++) {
+        std::cout << "\n";
+        for (int x = 0; x < nx; x++) {
+            std::cout << r_rec_sum[y][x] << " ";
+        }
+    }
     Result result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
     return result;
 }
