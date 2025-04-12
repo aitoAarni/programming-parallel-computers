5772f8180a05e430c3084d94e9d85a7fca7a65a5
aitoAarni <aarni.rechardt@gmail.com>
2025-04-12 23:18:40 +0200

    mf1 passed

--- mf1/mf.cc
+++ mf1/mf.cc
@@ -13,17 +13,14 @@ This is the function you need to implement. Quick reference:
 void mf(int ny, int nx, int hy, int hx, const float *in, float *out) {
   for (int y = 0; y < ny; y++) {
     for (int x = 0; x < nx; x++) {
-      std::vector<int> range;
-      std::cout << "\n\n\n" << x << " " << y << "\n";
+      std::vector<double> range;
       for (int j = y - hy; j <= y + hy && j < ny; j++) {
         if (j < 0) continue;
         for (int i = x - hx; i <= x + hx && i < nx; i++) {
           if (i < 0) continue;
           range.push_back(in[i+nx*j]);
-          std::cout << in[i+nx*j] << " ";
         }
       }
-      std::cout << "\n";
       double median;
       if (range.size() % 2 == 1) {
         int mid = range.size() / 2;
@@ -34,20 +31,9 @@ void mf(int ny, int nx, int hy, int hx, const float *in, float *out) {
         std::nth_element(range.begin(), range.begin() +  mid, range.end());
         double median1 = range[mid];
         std::nth_element(range.begin(), range.begin() +  --mid, range.end());
-        std::cout << "mid: " << mid << " ";
         median = (range[mid] + median1) / (double)2;
       }
       out[x + y * nx] = median;
-      std::cout << "median: " << median;
     }
   }
-}
-
-int main() {
-  float in[4] = {0, 0, 1, 4};
-  float out[4];
-
-  mf(2, 2, 1, 1, in, out);
-
-  return 0;
 }
