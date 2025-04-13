68e2228a8b3b274485d697106e944cddf3c165b6
aitoAarni <aarni.rechardt@gmail.com>
2025-04-13 22:24:47 +0200

    parallelism attempt

--- cp2a/cp.cc
+++ cp2a/cp.cc
@@ -1,8 +1,6 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-double zeroNormalized[16000000];
-double squareNormalized[16000000];
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -14,6 +12,8 @@ This is the function you need to implement. Quick reference:
 void correlate(int ny, int nx, const float *data, float *result) {
     std::vector<double> means(ny, 0.0);
     std::vector<double> squareSums(ny, 0.0);
+    std:: vector<double> zeroNormalized(16000000, 0.0);   
+    std::vector<double> squareNormalized(16000000, 0.0);
     for (int y = 0; y < ny; y++) {
         double sum = 0;
         for (int x = 0; x < nx; x++) {
@@ -38,12 +38,20 @@ void correlate(int ny, int nx, const float *data, float *result) {
         }
     }
 
+
     for (int j = 0; j < ny; j++) {
         for (int i = j; i < ny; i++) {
             double sum = 0;
-            for (int x = 0; x < nx; x++) {
-                sum += squareNormalized[x + nx * j] * squareNormalized[x+ nx * i];
+            int x;
+            for (x = 0; x + 3 < nx; x += 4) {
+                sum += squareNormalized[x + nx * j] * squareNormalized[x + nx * i];
+                sum += squareNormalized[x + 1 + nx * j] * squareNormalized[x + 1 + nx * i];
+                sum += squareNormalized[x + 2 + nx * j] * squareNormalized[x + 2 + nx * i];
+                sum += squareNormalized[x + 3 + nx * j] * squareNormalized[x + 3 + nx * i];
             }
+            for (; x < nx; x++) {
+                sum += squareNormalized[x + nx * j] * squareNormalized[x + nx * i];
+}
             result[i + j * ny] = (float)(sum);
         }
     }
