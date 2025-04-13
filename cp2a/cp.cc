63da817337b63469775923bbbb7cb1d788aef319
aitoAarni <aarni.rechardt@gmail.com>
2025-04-13 21:30:54 +0200

    starting point for cp2a

---
+++ cp2a/cp.cc
@@ -0,0 +1,50 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+double zeroNormalized[16000000];
+double squareNormalized[16000000];
+/*
+This is the function you need to implement. Quick reference:
+- input rows: 0 <= y < ny
+- input columns: 0 <= x < nx
+- element at row y and column x is stored in data[x + y*nx]
+- correlation between rows i and row j has to be stored in result[i + j*ny]
+- only parts with 0 <= j <= i < ny need to be filled
+*/
+void correlate(int ny, int nx, const float *data, float *result) {
+    std::vector<double> means(ny, 0.0);
+    std::vector<double> squareSums(ny, 0.0);
+    for (int y = 0; y < ny; y++) {
+        double sum = 0;
+        for (int x = 0; x < nx; x++) {
+            sum += data[x+y*nx];
+        }
+        means[y] = sum / nx;
+    }
+    
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+           zeroNormalized[x+y*nx] = data[x+y*nx] - means[y]; 
+        }
+    }
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            squareSums[y] += std::pow(zeroNormalized[x+y*nx], 2);           
+        }
+    }
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            squareNormalized[x + y * nx] = zeroNormalized[x+y*nx] / std::sqrt(squareSums[y]);
+        }
+    }
+
+    for (int j = 0; j < ny; j++) {
+        for (int i = j; i < ny; i++) {
+            double sum = 0;
+            for (int x = 0; x < nx; x++) {
+                sum += squareNormalized[x + nx * j] * squareNormalized[x+ nx * i];
+            }
+            result[i + j * ny] = (float)(sum);
+        }
+    }
+}
