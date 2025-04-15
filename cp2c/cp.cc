6ea40b43edbab039a874bc1bd7da17cfd53f4dfb
aitoAarni <aarni.rechardt@gmail.com>
2025-04-16 00:57:33 +0200

    save point

--- cp2c/cp.cc
+++ cp2c/cp.cc
@@ -5,6 +5,15 @@ double zeroNormalized[16000000];
 double squareNormalized[16000000];
 typedef double double4_t __attribute__ ((vector_size (4 * sizeof(double))));
 constexpr double4_t d4zero = {0, 0, 0, 0};
+
+
+double4_t sqrt_vector(double4_t v) {
+    double4_t result;
+    for (int i = 0; i < 4; ++i) {
+        result[i] = std::sqrt(v[i]);
+    }
+    return result;
+}
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -84,8 +93,27 @@ void correlate(int ny, int nx, const float *data, float *result) {
         for (int x = 0; x < newX; x++) {
             d[x + y * newX] = d[x + y * newX] - mean;
         }
+        for (int x = 1; x < 4; x++) {
+            if (4 * (newX - 1) + x >= nx) d[newX * 4 + y * newX][x] = 0;
+        }
+        double4_t c = d[0 + y * newX];
+        
+        std::cout << "norm0: " << c[0] << " " <<c[1] << " " <<c[2] << " " <<c[3] << "\n";
+        double4_t squareSums = d4zero;
         for (int x = 0; x < newX; x++) {
-            d[x + y * newX] = d[x + y * newX] * d[x + y * newX];
+            double4_t a = d[x + y * newX] * d[x + y * newX];
+            squareSums = squareSums + a;
+        }
+        double squareSum = 0;
+        for (int i = 0; i < 4; i++) {
+            squareSum += squareSums[i];
+        }
+        double4_t denominator = {squareSum, squareSum, squareSum, squareSum};
+        for (int x = 0; x < newX; x++) {
+            d[x + y * newX] = d[x + y * newX] / sqrt_vector(denominator);
+        }
+        for (int x = 1; x < 4; x++) {
+            if (4 * (newX - 1) + x >= nx) d[newX * 4 + y * newX][x] = 0;
         }
     }
     std::cout << "\n vec stand\n";
