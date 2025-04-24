fef6bfee0c7bde90c6e4f630d1bc0a3ad62d2773
aitoAarni <aarni.rechardt@gmail.com>
2025-04-24 22:59:36 +0200

    start for cp4

---
+++ cp4/cp.cu
@@ -0,0 +1,89 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <cuda_runtime.h>
+
+static inline void check(cudaError_t err, const char* context) {
+    if (err != cudaSuccess) {
+        std::cerr << "CUDA error: " << context << ": "
+            << cudaGetErrorString(err) << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+#define CHECK(x) check(x, #x)
+double zeroNormalized[16000000];
+double squareNormalized[16000000];
+/*
+This is the function you need to implement. Quick reference:
+- input rows: 0 <= y < ny
+- input columns: 0 <= x < nx
+- element at row y and column x is stored in data[x + y*nx]
+- correlation between rows i and row j has to be stored in result[i + j*ny]
+- only parts with 0 <= j <= i < ny need to be filled
+*/
+
+__global__ void mykernel(int ny, int nx, const float *data, float *result) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if (j >= ny || i >= ny || j > i) return;
+
+    float sum = 0;
+    for (int x = 0; x < nx; x++) {
+        sum += data[x + nx * j] * data[x+ nx * i];
+    }
+    result[i + j * ny] = (float)(sum);
+}
+
+static inline int divup(int a, int b) {
+    return (a + b - 1)/b;
+}
+
+static inline int roundup(int a, int b) {
+    return divup(a, b) * b;
+}
+
+void correlate(int ny, int nx, const float *data, float *result) {
+    std::vector<double> means(ny, 0.0);
+    std::vector<double> squareSums(ny, 0.0);
+    for (int y = 0; y < ny; y++) {
+        double sum = 0;
+        for (int x = 0; x < nx; x++) {
+            sum += data[x+y*nx];
+        }
+        means[y] = sum / nx;
+    }
+    
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+           zeroNormalized[x+y*nx] = data[x+y*nx] - means[y]; 
+        }
+    }
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            squareSums[y] += std::pow(zeroNormalized[x+y*nx], 2);           
+        }
+    }
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            squareNormalized[x + y * nx] = zeroNormalized[x+y*nx] / std::sqrt(squareSums[y]);
+        }
+    }
+
+    float* dGPU = NULL;
+    CHECK(cudaMalloc((void**)&dGPU, nx * ny * sizeof(float)));
+    float* rGPU = NULL;
+    CHECK(cudaMalloc((void**)&rGPU, ny * ny * sizeof(float)));
+    CHECK(cudaMemcpy(dGPU, squareNormalized, nx * ny * sizeof(float), cudaMemcpyHostToDevice));
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid(divup(ny, dimBlock.x), divup(ny, dimBlock.y));
+    mykernel<<<dimGrid, dimBlock>>>(ny, nx, dGPU ,rGPU);
+    CHECK(cudaGetLastError());
+
+    CHECK(cudaMemcpy(result, rGPU, ny * nx * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(dGPU));
+    CHECK(cudaFree(rGPU));
+
+}
