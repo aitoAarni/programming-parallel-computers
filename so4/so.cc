d1e9ea594c3a4b7c59ab9c70a0e7d698f7e842c5
aitoAarni <aarni.rechardt@gmail.com>
2025-04-27 23:19:23 +0200

    faster solution

--- so4/so.cc
+++ so4/so.cc
@@ -1,30 +1,28 @@
 #include <algorithm>
-#include <vector>
 #include <iostream>
 typedef unsigned long long data_t;
 
+data_t tempData[100000000];
 
 void merge(int start, int mid, int end, data_t *data) {
     int leftP = 0;
     int rightP = 0;
     int leftEnd = mid - start + 1;
     int rightEnd = end - mid;
-    std::vector<data_t> dataLeft(leftEnd);
-    std::vector<data_t> dataRight(rightEnd);
     for (int i = 0; i < leftEnd; i++) {
-        dataLeft[i] = data[i + start];
+        tempData[i + start] = data[i + start];
     }
     for (int i = 0; i < rightEnd; i++) {
-        dataRight[i] = data[i + mid + 1];
+        tempData[i + mid + 1] = data[i + mid + 1];
     }
     int i = start;
     for (; i <= end; i++) {
-        if (dataLeft[leftP] < dataRight[rightP]){ 
-            data[i] = dataLeft[leftP];
+        if (tempData[leftP + start] < tempData[rightP + mid + 1]){ 
+            data[i] = tempData[leftP + start];
             leftP++;
             if (leftP == leftEnd) break;
         } else {
-            data[i] = dataRight[rightP];
+            data[i] = tempData[rightP + mid + 1];
             rightP++;
             if (rightP == rightEnd) break;
         }
@@ -32,12 +30,12 @@ void merge(int start, int mid, int end, data_t *data) {
 
     for (int j = leftP; j < leftEnd; j++) {
         i++;
-        data[i] = dataLeft[j];
+        data[i] = tempData[j + start];
     }
     
     for (int j = rightP; j < rightEnd; j++) {
         i++;
-        data[i] = dataRight[j];
+        data[i] = tempData[j + mid + 1];
     }
     
 }
